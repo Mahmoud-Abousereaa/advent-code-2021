ce4b22bfdbc26a01430fee11907f871c405f26be
Mahmoud Abousereaa <mahmoud.abouseree3@gmail.com>
2021-12-19 20:30:39 +0000

    Day #1 Puzzle #2

--- day-1-puzzle-2/src/main.rs
+++ day-1-puzzle-2/src/main.rs
@@ -17,31 +17,34 @@ fn get_large_measurments() -> Result<i32, Box<dyn Error>> {
   let reader = BufReader::new(file);
   let mut count = 0;
   let mut current_line = 0;
-  let mut previous_three_measurements = 0;
-  let mut current_three_measurements = 0;
+  let mut previous_three_measurements: Vec<i32> = vec![];
+  let mut current_three_measurements: Vec<i32> = vec![];
   for line in reader.lines() {
-    if current_line >= 4 {
-      println!("previous_three_measurements: {}", previous_three_measurements);
-      println!("current_three_measurements: {}", current_three_measurements);
-      let diff = previous_three_measurements - current_three_measurements;
-      if previous_three_measurements < current_three_measurements {
-        count += 1;
-      }
-
-      previous_three_measurements = current_three_measurements;
-      current_three_measurements = current_three_measurements - diff;
-    } else {
-      previous_three_measurements = current_three_measurements;
-    }
-
-    if current_line > 4 {
-      break;
-    }
-
     let measurement_line = line.unwrap();
     let measurement: i32 = measurement_line.trim().parse().expect("Measurement is not available");
 
-    current_three_measurements += measurement;
+    if current_line < 1 {
+      previous_three_measurements.push(measurement);
+    } else if current_line < 3 {
+      previous_three_measurements.push(measurement);
+      current_three_measurements.push(measurement);
+    } else {
+      current_three_measurements.push(measurement);
+    }
+
+    if current_line >= 3 {
+      let previous_measurement_sum: i32 = previous_three_measurements.iter().sum();
+      let current_measurement_sum: i32 = current_three_measurements.iter().sum();
+
+      if previous_measurement_sum < current_measurement_sum {
+        count += 1;
+      }
+
+      let prev_measurement = current_three_measurements[2].clone();
+      previous_three_measurements.push(prev_measurement);
+      previous_three_measurements.remove(0);
+      current_three_measurements.remove(0);
+    }
 
     current_line += 1;
   }
